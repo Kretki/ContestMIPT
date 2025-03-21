7fb86935286ef9c309a1e1592fbc31731ab242d3
Kretki <grigoriotvext@mail.ru>
2025-03-22 01:28:24 +0300

    version_2

--- sql_interface.py
+++ sql_interface.py
@@ -1,48 +1,186 @@
-import pandas as pd
+import sqlite3
 
-class SqlExercisesInterface:
-    __db : pd.DataFrame
-    def __init__(self):
-        self.__db = pd.read_csv('db.csv', sep=';')
-        self.__db.set_index('id', drop=True, inplace=True)
-    def save_db(self):
-        self.__db.to_csv('db.csv', sep=';')
-    def add(self, number : int, text : str, text_answers : str = "", 
-                     value_answer : float = 0., unittest_answer : str = "", 
-                     type : int = 0, contest_id : int = 0):
-        # type = 0 -> задача с вариантами ответов, type = 1 -> задача на ввод значения, type = 2 -> задача на код
-        self.__db.loc[len(self.__db)] = [number, text, text_answers, value_answer, unittest_answer, type, contest_id]
-        self.save_db()
-        return len(self.__db) - 1
-    def delete(self, id : int):
-        if id in self.__db.index:
-            self.__db.drop([id], inplace=True)
-            self.save_db()
-    def change(self, id : int, number : int = -1, text : str = "", text_answers : str = "", 
-                     value_answer : float = -1., unittest_answer : str = "", 
-                     type : int = -1, contest_id : int = -1):
-        if id in self.__db.index:
-            row = self.__db.iloc[id].copy(deep=True)
-            if number != -1: row['number'] = number
-            if text != "": row['text'] = text
-            if text_answers != "": row['text_answers'] = text_answers
-            if value_answer != -1.: row['value_answer'] = value_answer
-            if unittest_answer != "": row['unittest_answer'] = unittest_answer
-            if type != -1: row['type'] = type
-            if contest_id != -1: row['contest_id'] = contest_id
-            self.__db.loc[id] = row
-            self.save_db()
-        else:
-            raise KeyError('No such id in the db to change') 
-    def get(self, id : int):
-        if id in self.__db.index:
-            return self.__db.iloc[id].copy(deep=True)
-        else:
-            raise KeyError('No such id in the db to get') 
+class DataBaseInterface:
+    def __init__(self, db_name="er-database.db"):
+        self.conn = sqlite3.connect(db_name, check_same_thread=False)
+        self.conn.execute("PRAGMA journal_mode=WAL;")  # Ensures better write concurrency
+        self.cursor = self.conn.cursor()
 
-if __name__ == '__main__':
-    sql = SqlExercisesInterface()
-    id = sql.add(1, 1, "Hello")
-    sql.change(id, type=2)
-    print(sql.get(id))
-    sql.delete(id)
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"User"} (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nickname TEXT,
+            login TEXT,
+            password TEXT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"GlobalUserStatistics"} (
+            run_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            right_answer INT,
+            wrong_answer INT,
+            user_id INT,
+            ex_id INT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"Contest"} (
+            contest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            contest_name TEXT,
+            contest_desc TEXT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"ContestExs"} (
+            UniqueID INTEGER PRIMARY KEY AUTOINCREMENT,
+            contest_id INT,
+            ex_id INT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"Exersises"} (
+            contest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ex_number INT,
+            ex_desc TEXT, 
+            ex_type INT,
+            right_answers INT,
+            wrong_answers INT,
+            answer_text TEXT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"Tests"} (
+            unittest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ex_id INT,
+            test_code TEXT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {"Administrator"} (
+            admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nickname TEXT,
+            login TEXT,
+            password TEXT
+        )
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def add_user(self, nickname, login, password):
+        query = "INSERT INTO User (nickname, login, password) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (nickname, login, password))
+        self.conn.commit()
+    
+    def add_test(self, ex_id, test_code):
+        query = "INSERT INTO Tests (ex_id, test_code) VALUES (?, ?)"
+        self.cursor.execute(query, (ex_id, test_code))
+        self.conn.commit()
+    
+    def add_contest_ex(self, contest_id, ex_number, ex_desc, ex_type, answer_text):
+        query = "INSERT INTO ContestExs (contest_id, ex_number, ex_desc, ex_type, answer_text) VALUES (?, ?, ?, ?, ?)"
+        self.cursor.execute(query, (contest_id, ex_number, ex_desc, ex_type, answer_text))
+        self.conn.commit()
+
+    def add_contest(self, contest_name, contest_desc):
+        query = '''INSERT INTO Contest (contest_name, contest_desc) VALUES (?, ?)'''
+        self.cursor.execute(query, (contest_name, contest_desc))
+        self.conn.commit()
+    
+    def add_admin(self, nickname, login, password):
+        query = "INSERT INTO Administrator (nickname, login, password) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (nickname, login, password))
+        self.conn.commit()
+    
+    def get_user(self, username, password):
+        query = "SELECT * FROM User WHERE login =? AND password =?"
+        self.cursor.execute(query, (username, password))
+        return self.cursor.fetchone()
+    
+    def get_admin(self, username, password):
+        query = "SELECT * FROM Administrator WHERE login =? AND password =?"
+        self.cursor.execute(query, (username, password))
+        return self.cursor.fetchone()
+    
+    def get_contest_exs(self, contest_id):
+        query = "SELECT * FROM ContestExs WHERE contest_id =?"
+        self.cursor.execute(query, (contest_id,))
+        return self.cursor.fetchall()
+    
+    def get_tests(self, ex_id):
+        query = "SELECT * FROM Tests WHERE ex_id =?"
+        self.cursor.execute(query, (ex_id,))
+        return self.cursor.fetchall()
+    
+    def get_contest_by_id(self, contest_id):
+        query = "SELECT * FROM Contest WHERE contest_id =?"
+        self.cursor.execute(query, (contest_id,))
+        return self.cursor.fetchone()
+    
+    def get_contest_by_name(self, contest_name):
+        query = "SELECT * FROM Contest WHERE contest_name =?"
+        self.cursor.execute(query, (contest_name,))
+        return self.cursor.fetchone()
+    
+    def get_global_stats(self, user_id):
+        query = "SELECT * FROM GlobalUserStatistics WHERE user_id =?"
+        self.cursor.execute(query, (user_id,))
+        return self.cursor.fetchall()
+    
+    def update_global_stats(self, user_id, ex_id, right_answer, wrong_answer):
+        query = '''INSERT INTO GlobalUserStatistics (right_answer, wrong_answer, user_id, ex_id) VALUES (?,?,?,?) 
+        ON CONFLICT (user_id, ex_id) 
+        DO UPDATE SET right_answer =?, wrong_answer =?'''
+        self.cursor.execute(query, (right_answer, wrong_answer, user_id, ex_id, right_answer, wrong_answer))
+
+    def update_contest(self, contest_id, contest_name, contest_desc):
+        query = "UPDATE Contest SET contest_name =?, contest_desc =? WHERE contest_id =?"
+        self.cursor.execute(query, (contest_name, contest_desc, contest_id))
+        self.conn.commit()
+    
+    def delete_user(self, user_id):
+        query = "DELETE FROM User WHERE user_id =?"
+        self.cursor.execute(query, (user_id,))
+        self.conn.commit()
+        query = "DELETE FROM GlobalUserStatistics WHERE user_id =?"
+        self.cursor.execute(query, (user_id,))
+        self.conn.commit()
+
+    def delete_admin(self, admin_id):
+        query = "DELETE FROM Administrator WHERE admin_id =?"
+        self.cursor.execute(query, (admin_id,))
+        self.conn.commit()
+
+    def delete_ex(self, ex_id):
+        query = "DELETE FROM Exersises WHERE contest_id =?"
+        self.cursor.execute(query, (ex_id,))
+        self.conn.commit()
+    
+    def delete_test(self, unittest_id):
+        query = "DELETE FROM Tests WHERE unittest_id =?"
+        self.cursor.execute(query, (unittest_id,))
+        self.conn.commit()
+
+    def delete_contest_ex(self, contest_id, ex_id):
+        query = "DELETE FROM ContestExs WHERE contest_id =? AND ex_id =?"
+        self.cursor.execute(query, (contest_id, ex_id))
+        self.conn.commit()
+
+    
